6eb992a5526d4abc90a13e3a91ae826e18ad8a6a
RAJAN PRAJAPATI <113326646+rajanprajapati1@users.noreply.github.com>
2024-08-20 22:38:13 +0530

    Update useDeounceHook.js

--- useDeounceHook.js
+++ useDeounceHook.js
@@ -26,3 +26,90 @@ const useDebounce = (value, delay) => {
 };
 
 export default useDebounce;
+
+
+
+uesage
+
+import React, { useState } from "react";
+import useDebounce from './components/UseDebounce';
+
+const LetOut = ({ onChange }) => {
+  const [priceRange, setPriceRange] = useState({ min: 0, max: 10000 });
+  
+  // Use the useDebounce hook to get debounced values
+  const debouncedRange = {
+    min: useDebounce(priceRange.min, 1000),
+    max: useDebounce(priceRange.max, 1000),
+  };
+
+  // Notify parent component of the debounced values
+  React.useEffect(() => {
+    if (onChange) {
+      onChange([debouncedRange.min, debouncedRange.max]);
+    }
+  }, [debouncedRange, onChange]);
+
+  // Handle changes for both range and input fields
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setPriceRange((prevRange) => ({
+      ...prevRange,
+      [id]: Number(value),
+    }));
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <label>
+        Min Price:
+        <input
+          id="min"
+          type="range"
+          min="0"
+          max="10000"
+          step="100"
+          value={priceRange.min}
+          onChange={handleChange}
+        />
+        <input
+          id="min"
+          type="number"
+          min="0"
+          max="10000"
+          value={priceRange.min}
+          onChange={handleChange}
+        />
+        ₹{priceRange.min}
+      </label>
+      <br />
+      <label>
+        Max Price:
+        <input
+          id="max"
+          type="range"
+          min="0"
+          max="10000"
+          step="100"
+          value={priceRange.max}
+          onChange={handleChange}
+        />
+        <input
+          id="max"
+          type="number"
+          min="0"
+          max="10000"
+          value={priceRange.max}
+          onChange={handleChange}
+        />
+        ₹{priceRange.max}
+      </label>
+      <br />
+      <div>
+        Selected Range: ₹{debouncedRange.min} - ₹{debouncedRange.max}
+      </div>
+    </div>
+  );
+};
+
+export default LetOut;
